140947dd9eb5d20d6ec7fba25c4f0315ccfff6f7
James <james@jemz.org>
2016-12-28 22:34:22 +0700

    image search

--- server.js
+++ server.js
@@ -2,9 +2,61 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.get('/', function(req, res){
-	res.send('image search project');
-})
+var ImageClient = require('google-images');
+let client = new ImageClient('011588903241061341004:us5qbfn9dk4', 'AIzaSyA1QC4HjyrVtzovkah244-K5DShu7DLrME');
+
+
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var url = 'mongodb://localhost:27017/querydb'
+
+
+mongoClient.connect(url, function(err, db){
+   if (err) {
+   	console.log('Unable to connect to mongodb server. Error: ' + err);
+   } else {
+   	console.log('Connection established to ', url);
+
+   	var collection = db.collection('query');
+		app.get('/api/imagesearch', function(req, res){
+
+			var uri = req.url;
+		    var input = uri.toString().substring(19, uri.toString().length); //remove the junk
+
+			var newDate = new Date().toLocaleString();
+		    var row = {
+		    	term: input,
+		    	time:  newDate
+		    }
+
+		    collection.insert(row, function(err, result){
+		    	if(err) {
+		    		console.log(err);
+		    	} else {
+		    		console.log('Inserted %d documents into the "redirect" collection. the document inserted with "_id" is ', result.length, result); 
+		    	}
+		    })
+
+			client.search(input)
+		    .then(function (images) {
+		    	res.send(images);
+		    });	
+		})
+
+		app.get('/api/latest/imagesearch', function(req, res) {
+			collection.find().toArray(function(err, result){
+				if (err) {
+					console.log(err);
+				} else if (result.length){
+					res.send(result);
+				}
+			})
+		});
+
+   }
+});
+
+
 
 app.listen(port, function(){
 	console.log('image search app is running at port ' + port);
